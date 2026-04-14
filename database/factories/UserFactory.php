10a3697b924064f57c64a259bb10b45c4af967c6
Muhamad Zaqi Wiradinata <mzqwrdnta@gmail.com>
2026-04-14 16:10:10 +0700

    Use Faker instance for generating user names

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 /**
  * @extends Factory<User>
@@ -24,8 +25,9 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = Faker::create();
         return [
-            'name' => $this->faker->name(),
+            'name' => $faker->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
